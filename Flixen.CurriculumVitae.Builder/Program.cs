d07083d7265205d6d44f6f179fb9d0e71bf4eeba
Felix Svensson <klowdo.fs@gmail.com>
2024-09-27 18:52:21 +0200

    feat: add write and live command line

--- Flixen.CurriculumVitae.Builder/Program.cs
+++ Flixen.CurriculumVitae.Builder/Program.cs
@@ -1,26 +1,66 @@
 ﻿using Flixen.CurriculumVitae.Contracts;
 using Flixen.CurriculumVitae.Layouts;
 using QuestPDF.Drawing;
+using System.CommandLine;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
 using QuestPDF.Previewer;
 using SkiaSharp.Extended.Iconify;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
+QuestPDF.Settings.License = LicenseType.Community;
 
-var deserializer = new DeserializerBuilder()
-    .WithNamingConvention(CamelCaseNamingConvention.Instance) // see height_in_inches in sample yml 
-    .Build();
+var fileOption = new Option<FileInfo>(
+    name: "--config-file",
+    description: "The path to the configuration file",
+    getDefaultValue: () =>  new FileInfo("./data.yaml")
+);
 
-var path = args.Length > 0 ? args[0] : "./data.yaml";
-using var reader = File.OpenRead(path);
-using var stream = new StreamReader(reader);
-var options = deserializer.Deserialize<ResumeModel>(stream);
+var outputOption = new Option<FileInfo>(
+    name: "--output",
+    description: "The path to the configuration file"
+) { IsRequired = true };
 
-FontLoader.LoadFront(options.Fonts);
-SKTextRunLookup.Instance.AddFontAwesome();
-QuestPDF.Settings.CheckIfAllTextGlyphsAreAvailable = true;
-FontManager.RegisterFont(FontAwesome.GetFontStream());
-var document = new DefaultCvDocument(options);
+var liveCommand = new Command("live", "Starts a live preview of the resume");
+var writeCommand = new Command("write", "Writes the resume to a file");
+writeCommand.AddOption(outputOption);
+var rootCommand = new RootCommand("A command line tool to generate a PDF resume")
+{
+    liveCommand,
+    writeCommand
+};
+rootCommand.AddOption(fileOption);
 
+liveCommand.SetHandler((configFile) =>
+{
+    var document = CreateDocument(configFile);
+    document.ShowInPreviewer();
+}, fileOption);
 
-document.ShowInPreviewer();
+writeCommand.SetHandler((configFile, output) =>
+{
+    var document = CreateDocument(configFile);
+    output.Directory?.Create();
+    document.GeneratePdf(output.FullName);
+}, fileOption, outputOption);
+
+return await rootCommand.InvokeAsync(args); 
+
+DefaultCvDocument CreateDocument(FileInfo configFile)
+{
+    var deserializer = new DeserializerBuilder()
+        .WithNamingConvention(CamelCaseNamingConvention.Instance) // see height_in_inches in sample yml 
+        .Build();
+
+    using var reader = configFile.OpenRead();
+    using var stream = new StreamReader(reader);
+    var options = deserializer.Deserialize<ResumeModel>(stream);
+
+    FontLoader.LoadFront(options.Fonts);
+    SKTextRunLookup.Instance.AddFontAwesome();
+    QuestPDF.Settings.CheckIfAllTextGlyphsAreAvailable = true;
+    FontManager.RegisterFont(FontAwesome.GetFontStream());
+    var defaultCvDocument = new DefaultCvDocument(options);
+    return defaultCvDocument;
+}
